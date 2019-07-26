08dda5e5e06cb3239e48a71df2c2780fe6e082e7
sishida21 <ishida.shouichi.57a@st.kyoto-u.ac.jp>
2019-07-26 15:25:38 +0900

    refactor: use /home/acb10811dh and update README.md

--- qsub_command.sh
+++ qsub_command.sh
@@ -1,5 +1,5 @@
 x=`pwd`
 d=`date +%Y%m%d%H%M`
-sed -e "s#<command>#$*#g" /home/aca10350zi/qsub_template.sh | sed -e "s#<pwd>#${x}#g" > ~/tmp/cmd.${d}.$$.sh
-qsub -g gca50095 ~/tmp/cmd.${d}.$$.sh
+sed -e "s#<command>#$*#g" ${HOME}/qsub_template.sh | sed -e "s#<pwd>#${x}#g" > ${HOME}/tmp/cmd.${d}.$$.sh
+qsub -g gca50095 ${HOME}/tmp/cmd.${d}.$$.sh
 
